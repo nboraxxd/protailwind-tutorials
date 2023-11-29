8ca074c82906e95aae3128315b0ae85bc6981954
bora <nhatbaoxxd@gmail.com>
2023-11-29 11:01:48 +0700

    Transform CSS Into a Dynamic Tailwind Plugin

--- src/tutorials/animated-stripes/01-06-02/stripes-plugin.cjs
+++ src/tutorials/animated-stripes/01-06-02/stripes-plugin.cjs
@@ -1,8 +1,39 @@
 const plugin = require('tailwindcss/plugin')
 
-module.exports = plugin(function () {
-  /*
-    TODO:
-    Port over the stripes CSS from `./tailwind.css`.
-  */
+module.exports = plugin(function ({ addBase, addComponents, addUtilities }) {
+  addBase({
+    '@keyframes slide': {
+      from: { transform: 'translateX(0)' },
+      to: { transform: 'translateX(20px)' },
+    },
+    ':root': { '--stripes-color': 'black', '--stripes-angle': '-45deg' },
+  })
+
+  addComponents({
+    '.stripes-practice': { position: 'relative', overflow: 'hidden' },
+    '.stripes-practice > *': { isolation: 'isolate' },
+    '.stripes-practice:before': {
+      position: 'absolute',
+      top: '0',
+      right: '0',
+      height: '100%',
+      width: 'calc(100% + 20px)',
+      content: "''",
+      backgroundImage: `linear-gradient(
+          var(--stripes-angle),
+          var(--stripes-color) 5%,
+          transparent 5% 45%,
+          var(--stripes-color) 45% 55%,
+          transparent 55% 95%,
+          var(--stripes-color) 95%
+        )`,
+      backgroundSize: '20px 20px',
+      animation: 'slide 1s infinite linear',
+    },
+  })
+
+  addUtilities({
+    '.stripes-practice-white': { '--stripes-color': 'white' },
+    '.stripes-practice-reverse': { '--stripes-angle': '45deg' },
+  })
 })
